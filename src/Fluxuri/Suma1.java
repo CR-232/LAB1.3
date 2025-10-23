fa362670c46d3161515660b55d1b8a3c72c6066a
Tihon444 <victor.tihon@iis.utm.md>
2025-10-23 14:56:25 +0300

    w3w1e31

---
+++ src/Fluxuri/Suma1.java
@@ -0,0 +1,15 @@
+package Fluxuri;
+
+import java.util.List;
+
+public class Suma1 {
+    public Suma1(int i, int i1, List<Integer> numbers, int i2, String s) {
+    }
+
+    public void start() {
+    }
+
+    public void join() {
+
+    }
+}
