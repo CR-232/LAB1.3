d437754ae1eab1a60511ee01b74a1591018103b1
Tihon444 <victor.tihon@iis.utm.md>
2025-11-27 14:52:42 +0200

    lab3final

--- src/Main.java
+++ src/Main.java
@@ -1,161 +1,106 @@
 public class Main {
 
     static int n = 1000;
-    static int[] tabCalc = new int[n];
-    static int[] tabPar = new int[n];
+    static int[] data = new int[n];
 
-    //LOCK GLOBAL pentru absolut toate afișările
-    static final Object printLock = new Object();
-
-    static FirCalcInce Th1 = new FirCalcInce();
-    static FirCalcSfar Th2 = new FirCalcSfar();
-    static FirParcurgereCrescator Th3 = new FirParcurgereCrescator();
-    static FirParcurgereDesc Th4 = new FirParcurgereDesc();
+    static Th1 fir1 = new Th1();
+    static Th2 fir2 = new Th2();
+    static Th3 fir3 = new Th3();
+    static Th4 fir4 = new Th4();
 
     public static void main(String[] args) {
 
         for (int i = 0; i < n; i++) {
-            tabCalc[i] = (int) (Math.random() * 1000);
-            tabPar[i] = i;
+            data[i] = (int)(Math.random()*1000);
         }
 
-        Th1.setName("Th1");
-        Th2.setName("Th2");
-        Th3.setName("Th3");
-        Th4.setName("Th4");
+        fir1.setName("Th1");
+        fir2.setName("Th2");
+        fir3.setName("Th3");
+        fir4.setName("Th4");
 
-        Th1.start();
-        Th2.start();
-        Th3.start();
-        Th4.start();
+        // sinc th 1–2  join
+        fir1.start();
+        try { fir1.join(); } catch (Exception e) {}
+        fir2.start();
+        try { fir2.join(); } catch (Exception e) {}
+
+        // sinc th 3–4
+        fir3.setPriority(Thread.MAX_PRIORITY);
+        fir4.setPriority(Thread.MIN_PRIORITY);
+
+        fir3.start();
+        try { Thread.sleep(150); } catch(Exception e){}
+        fir4.start();
 
         try {
-            Th1.join();
-            Th2.join();
-            Th3.join();
-            Th4.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            fir3.join();
+            fir4.join();
+        } catch (Exception e) {}
+    }
+
+    // afisare lenta
+    static void printSlow(String txt){
+        for(char c : txt.toCharArray()){
+            System.out.print(c);
+            try{ Thread.sleep(100); } catch(Exception e){}
+        }
+        System.out.println();
+    }
+
+    static class Th1 extends Thread {
+        public void run(){
+            long sum = 0;
+            for(int x : data){
+                if(x % 2 == 0){
+                    sum += x;
+                }
+            }
+            System.out.println(getName() + " → Suma numerelor pare = " + sum);
+
+            printSlow(getName() + " → Prenume: Victor , Radu");
         }
     }
 
-    //Afișare lentă 100% protejată
-    public static void afisare(String text) {
-        synchronized (printLock) {
-            for (char c : text.toCharArray()) {
-                System.out.print(c);
-                try { Thread.sleep(100); } catch (Exception e) {}
+    static class Th2 extends Thread {
+        public void run(){
+            long sum = 0;
+            for(int x : data){
+                if(x % 2 != 0){
+                    sum += x;
+                }
             }
-            System.out.println();
+            System.out.println(getName() + " → Suma numerelor impare = " + sum);
+
+            printSlow(getName() + " → Nume: Tihon , Vlas");
         }
     }
 
-    //Afișare instant protejată
-    public static void printSync(String text) {
-        synchronized (printLock) {
-            System.out.println(text);
+    static class Th3 extends Thread {
+        public void run(){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 100; i <= 200; i++){
+                sb.append(data[i]).append(" ");
+                try{ Thread.sleep(1);} catch(Exception e){}
+            }
+            System.out.println(getName() + " → Segment [100..200]:");
+            System.out.println(sb.toString());
+
+            printSlow(getName() + " → Disciplina: Programare Concurenta si Distribuita");
         }
     }
 
-    static class FirCalcInce extends Thread {
-        public void run() {
-
-            int S = 0, C = 0, s1 = 0, s2 = 0, pair = 0;
-
-            for (int i = 0; i < n; i++) {
-                if (tabCalc[i] % 2 == 0) {
-                    S += tabCalc[i];
-                    C++;
-                }
-
-                if (C == 2) {
-                    pair++;
-                    if (pair == 1) s1 = S;
-                    else {
-                        s2 = S;
-                        printSync(getName() + " → Suma = " + (s1 + s2));
-                        pair = 0;
-                    }
-                    S = 0;
-                    C = 0;
-                }
-                try { Thread.sleep(1); } catch (Exception e) {}
+    static class Th4 extends Thread {
+        public void run(){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 900; i >= 800; i--){
+                sb.append(data[i]).append(" ");
+                try{ Thread.sleep(1);} catch(Exception e){}
             }
+            System.out.println(getName() + " → Segment [900..800]:");
+            System.out.println(sb.toString());
 
-            afisare(getName() + " Prenume: Victor, Radu");
-        }
-    }
-
-    static class FirCalcSfar extends Thread {
-        public void run() {
-
-            int S = 0, C = 0, s1 = 0, s2 = 0, pair = 0;
-
-            for (int i = n - 1; i >= 0; i--) {
-                if (tabCalc[i] % 2 == 0) {
-                    S += tabCalc[i];
-                    C++;
-                }
-
-                if (C == 2) {
-                    pair++;
-                    if (pair == 1) s1 = S;
-                    else {
-                        s2 = S;
-                        printSync(getName() + " → Suma = " + (s1 + s2));
-                        pair = 0;
-                    }
-                    S = 0;
-                    C = 0;
-                }
-                try { Thread.sleep(1); } catch (Exception e) {}
-            }
-
-            afisare(getName() + " Nume: Tihon, Vlas");
-        }
-    }
-
-    static class FirParcurgereCrescator extends Thread {
-        public void run() {
-
-            synchronized (printLock) {
-                for (int i = 100; i <= 500; i++) {
-                    System.out.print(tabPar[i] + " ");
-                    try { Thread.sleep(2); } catch (Exception e) {}
-                }
-                System.out.println();
-            }
-
-            try {
-                Th1.join();
-                Th2.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            afisare(getName() + " Disciplina: Programare Concurenta si Distribuita");
-        }
-    }
-
-    static class FirParcurgereDesc extends Thread {
-        public void run() {
-
-            synchronized (printLock) {
-                for (int i = 700; i >= 300; i--) {
-                    System.out.print(tabPar[i] + " ");
-                    try { Thread.sleep(2); } catch (Exception e) {}
-                }
-                System.out.println();
-            }
-
-            try {
-                Th1.join();
-                Th2.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            afisare(getName() + " Grupa: CR-232");
+            printSlow(getName() + " → Grupa: CR-232");
         }
     }
 }
